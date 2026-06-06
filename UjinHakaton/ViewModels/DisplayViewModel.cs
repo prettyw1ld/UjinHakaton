afcaa3cd3106271449d54389e5beaef511a3e78c
ekaterinasmertina <ekaterinasmertina@PEEB>
2026-06-06 22:55:50 +0500

    feat: Создание страницы выбора телевизора для передачи на него новостей и т.д.

---
+++ UjinHakaton/ViewModels/DisplayViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace UjinHakaton.ViewModels
+{
+    public partial class DisplayViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string? _selectedTV;
+
+        [ObservableProperty]
+        private string _errorMessage;
+
+        [ObservableProperty]
+        private bool _isErrorVisible;
+
+        public ObservableCollection<string> TV { get; } = new()
+        {
+            "DEXP OLED 55\" - 1920x1080 (Full HD)",
+            "DEXP OLED 55\" - 1920x1080 (Full HD)",
+            "DEXP OLED 55\" - 1920x1080 (Full HD)",
+            "DEXP OLED 55\" - 1920x1080 (Full HD)",
+            "DEXP OLED 55\" - 1920x1080 (Full HD)",
+            "DEXP OLED 55\" - 1920x1080 (Full HD)",
+            "DEXP OLED 55\" - 1920x1080 (Full HD)"
+        };
+
+        [RelayCommand]
+        private void ChooseTV()
+        {
+            if (SelectedTV == null)
+            {
+                ErrorMessage = "Выберите телевизор!";
+                IsErrorVisible = true;
+            }
+            else
+            {
+                IsErrorVisible = false;
+                if (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+                {
+                    var templateWindow = new TemplatesView();
+                    templateWindow.Show();
+
+                    desktop.MainWindow.Close();
+                }
+            }
+        }
+    }
+}
---
+++ UjinHakaton/Views/TemplatesView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace UjinHakaton;
+
+public partial class TemplatesView : Window
+{
+    public TemplatesView()
+    {
+        InitializeComponent();
+    }
+}
