36ee6ab2a3ece63f124b10d2d4bd858a2cfd1609
Damir <denikolop@mail.ru>
2026-06-06 13:34:23 +0500

    Add project files.

---
+++ UjinHakaton/Views/MainView.axaml.cs
@@ -0,0 +1,13 @@
+
+using Avalonia;
+using Avalonia.Controls;
+
+namespace UjinHakaton.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ UjinHakaton/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace UjinHakaton.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
