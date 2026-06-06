0880e893ea4a8cc2b85cecf3a91c8dc344375879
Алексей Шашин <alexshashin.2023@gmail.com>
2026-06-06 15:13:36 +0500

    feat: (Созданы модели для новостей)

---
+++ UjinHakaton/Models/API/NewsListResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UjinHakaton.Models.NewsModel;
+
+namespace UjinHakaton.Models.API
+{
+    public class NewsListResponse
+    {
+        public string Command { get; set; }
+        public string Message { get; set; }
+        public int Error { get; set; }
+        public List<NewsData> Items { get; set; }
+
+        public Links Links { get; set; }
+
+    }
+
+    public class NewsData
+    {
+        public List<NewsItem> Items { get; set; }
+
+    }
+    public class Links
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public object? Prev { get; set; }
+        public object? Next { get; set; }
+    }
+}
---
+++ UjinHakaton/Models/NewsModel/NewsItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UjinHakaton.Models.Builds;
+
+namespace UjinHakaton.Models.NewsModel
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+        public int Status { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Published_at { get; set; }
+        public DateTime? Expires_at { get; set; }
+        public DateTime? Schedule_for { get; set; }
+        public List<string> Images { get; set; }
+        public Type Type { get; set; }
+        public List<Buildings> Buildings { get; set; }
+        public int Remote_id { get; set; }
+        public bool Is_external { get; set; }
+        public object? Targeting { get; set; }
+    }
+}
