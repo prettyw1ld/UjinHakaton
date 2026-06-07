67dbb4935de5ccd4de0fae8ad7521a15a2c36f60
Damir <denikolop@mail.ru>
2026-06-07 11:58:53 +0500

    fix: вернул контроллер

---
+++ UjinTemplateServer/Controllers/TemplateController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UjinTemplateServer.Common;
+using UjinTemplateServer.Models;
+
+namespace UjinTemplateServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TemplateController(AppDbContext dbContext) : ControllerBase
+    {
+        public readonly AppDbContext _dbContext = dbContext;
+
+        [HttpGet]
+        public async Task<ActionResult<Template>> GetTemplatesAsync()
+        {
+            try
+            {
+                List<Template> templates = await _dbContext.Templates.ToListAsync();
+
+                return Ok(templates);
+
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+    }
+}
